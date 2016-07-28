63d5ce11c719372f67c8d84149d6eaf0edbf74af
skywalkeryin <skywalkeryin007@gmail.com>
2016-07-28 11:09:50 +0800

    modify SC  aspx.designer.cs

--- Team_2BookOnlineOrderSystem/View_Customer/ShoppingCart.aspx.designer.cs
+++ Team_2BookOnlineOrderSystem/View_Customer/ShoppingCart.aspx.designer.cs
@@ -58,15 +58,6 @@ namespace Team_2BookOnlineOrderSystem {
         protected global::System.Web.UI.WebControls.Label lblTotal;
         
         /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
         /// btnRemove control.
         /// </summary>
         /// <remarks>
